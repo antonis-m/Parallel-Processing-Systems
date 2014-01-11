b3f343e4649e120d2833f3079bf74786d582cd2f
antonis-m <antonismanousis@gmail.com>
2014-01-11 17:35:11 +0200

    radical change of the parallel code. need to fix final bug with gather and add counters

--- exe1/2013-2014/LU_block_p2p.c
+++ exe1/2013-2014/LU_block_p2p.c
@@ -58,45 +58,70 @@ int main (int argc, char * argv[]) {
     ******************************************************************************/
 
  
-
-/*for (i=0; i<2; i++){
-  for(j=0; j<2;j++)
-    printf("%f ",localA[i][j]);
-  printf("\n");
-}
+     block_size= x; //X_ext/size;
+/*     printf("block size %d\n",block_size);
+     printf("size %d\n",size);
+     printf("x %d\n",x);
+     printf("X_ext %d\n",X_ext);
 */
-     block_size= x/size;
-     printf("block size %d\n",block_size);
-     for (k=0; k<x-1; k++) {
-       if (rank==k/block_size) {
-         for(l=(k/block_size); l<size; l++){ //the iteration begins from the first non idle line.
-           if(l!=(k/block_size))
-             MPI_Send(&localA[k%x][0],y,MPI_DOUBLE,l,0,MPI_COMM_WORLD);   //FIXME bug with size to be sent
-        }
-        //computations
-         for (i=(k+1)%x; i<x; i++) {   
-           L=localA[i][k]/localA[k%x][k];
-           for (j=k; j<y; j++)
-             localA[i][j]-=L*localA[k%x][j];
-         }
+         for (k=0; k<(X_ext-1); k++) {   //X_ext anti gia x
+ //        printf("k= %d\n",k);
+         if (rank==k/block_size) {
+ //          printf("I M IN THIS SHIT\n");  
+ //          printf("RANK = %d\n",rank);
+           for(l=(k/block_size); l<size; l++){ //the iteration begins from the first non idle line.
+             if(l!=(k/block_size)) {
+               MPI_Send(&localA[k%x][0],y,MPI_DOUBLE,l,0,MPI_COMM_WORLD);   //FIXME optimize size to be sent
+   //            for (i=0; i<y; i++ )  printf("localA sent %f ",localA[k%x][i]);
+   //            printf("\n");
+             }
+           }
+
+           //computations
+           for (i=(k+1)%x; i<x; i++) {
+             if (i==0)
+                break;
+             else {  
+             L=localA[i][k]/localA[k%x][k];
+             for (j=k; j<y; j++){
+               localA[i][j]-=L*localA[k%x][j];
+     //          printf("new_localA %f ",localA[i][j]);
+             }
+            }
+           }
+        
        }  
 
        else if (rank > (k/block_size)) {
-         printf("rank %d\n",rank);
-
+      //   printf("rank %d\n",rank);
+         
          double * line_received ;
          line_received = (double *)malloc(y*sizeof(double));
-
-         MPI_Recv(&line_received,y,MPI_DOUBLE,k/block_size,0,MPI_COMM_WORLD,&stat);  // FIXME bug with size to be received
+         MPI_Recv(&line_received[0],y,MPI_DOUBLE,k/block_size,0,MPI_COMM_WORLD,&stat);  // FIXME 
+        // for (i=0; i<y; i++)
+        //      printf("linerec %f ",line_received[i]);
+         
          //computations
-         printf("Computations starting\n");
+        // printf("Computations starting. All lines affected\n");
          for (i=0; i<x; i++) {
+            // printf("\n");
            L=localA[i][k]/line_received[k];
            for (j=k; j<y; j++)
              localA[i][j]-=L*line_received[j];
+          // for (j=0; j<y; j++)   
+          //   printf("newlocalA %f ",localA[i][j]);
+             printf("\n");
+          
+          }
+        
 
-         }
-        }   
+        }  
+       printf("LOCALA\n");
+      for(i=0; i<x; i++) {
+        for (j=0; j<y; j++)
+            printf("%f ",localA[i][j]);
+        printf("\n");
+      } 
      }
 
 /*for (i=0; i<2; i++){
@@ -110,15 +135,15 @@ int main (int argc, char * argv[]) {
     gettimeofday(&tf,NULL);
     total_time=tf.tv_sec-ts.tv_sec+(tf.tv_usec-ts.tv_usec)*0.000001;
 	
-
+    printf("COMPUTATIONS FINISHED\n");
     //Gather local matrices back to the global matrix
     if (rank==0) {
         A=malloc2D(X,Y);    
         idx=&A[0][0];
     }
-    MPI_Gather(&localA[0][0],x*y,MPI_DOUBLE,idx,x*y,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    
-    double avg_total,avg_comp,avg_comm,max_total,max_comp,max_comm;
+    MPI_Gather(&localA[0],x*y,MPI_DOUBLE,idx,x*y,MPI_DOUBLE,0,MPI_COMM_WORLD); // FIXME provlima sto gather !!!!
+     printf("MPI GATHER COMPLETE\n");    
+/*    double avg_total,avg_comp,avg_comm,max_total,max_comp,max_comm;
     MPI_Reduce(&total_time,&max_total,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
     MPI_Reduce(&computation_time,&max_comp,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
     MPI_Reduce(&communication_time,&max_comm,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
@@ -135,7 +160,7 @@ int main (int argc, char * argv[]) {
         printf("Max times:\tTotal\t%lf\tComp\t%lf\tComm\t%lf\n",max_total,max_comp,max_comp);
         printf("Avg times:\tTotal\t%lf\tComp\t%lf\tComm\t%lf\n",avg_total,avg_comp,avg_comp);
     }
-
+*/
     //Print triangular matrix U to file
     if (rank==0) {
         char * filename="output_block_p2p";
