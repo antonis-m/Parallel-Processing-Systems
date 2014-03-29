349deb161a14256e0a3ddfa8f26e0e4ba806ed2b
Marios Kogias <marioskogias@grnet.gr>
2014-03-29 22:36:07 +0200

    Add a buggy version of tiled implementation

--- exe3/src/gpu_kernels.cu
+++ exe3/src/gpu_kernels.cu
@@ -44,8 +44,9 @@ graph_t *copy_graph_from_gpu(const weight_t *dist_gpu, graph_t *graph)
  */ 
 __global__ void GPU_KERNEL_NAME(_naive)(weight_t *dist, int n, int k)
 {
-    int tid = (blockDim.x*blockDim.y*blockIdx.x)+
-        (threadIdx.x*blockDim.y + threadIdx.y);  
+    int tid = (blockDim.x*blockDim.y*blockIdx.x)+ // consider line grid
+        (threadIdx.x*blockDim.y + threadIdx.y);   // and global order 
+
     if (tid > n*n)
         return;
 
@@ -59,54 +60,52 @@ __global__ void GPU_KERNEL_NAME(_naive)(weight_t *dist, int n, int k)
  *  The tiled GPU kernel(s) using global memory
  */ 
 __global__ void GPU_KERNEL_NAME(_tiled_stage_X)(weight_t *dist, int n,
-        int k_tile)
+        int k_tile, int kk)
 {
-    int tid = (blockDim.x*blockDim.y*blockIdx.x)+
-        (threadIdx.x*blockDim.y + threadIdx.y);  
-    if (tid > n*n)
-        return;
+    int tid = threadIdx.x*blockDim.y + threadIdx.y;  
+    
     weight_t * a;
     weight_t * b;
     weight_t * c;
     
-    /*case single block or line grid*/
+    /*case single block or column grid*/
     if (gridDim.x == 1) {
 
         /*signle block*/
         if (gridDim.y == 1) {
-            a = &dist[k_tile*GPU_TILE_DIM+k_tile*n];
-            b = &dist[k_tile*GPU_TILE_DIM+k_tile*n];
-            c = &dist[k_tile*GPU_TILE_DIM+k_tile*n];
-        } else { /*line grid*/
+            a = &dist[k_tile*GPU_TILE_DIM+k_tile*GPU_TILE_DIM*n]; // Tkk
+            b = a;
+            c = a;
+        } else { /*column grid*/
             if (blockIdx.y == k_tile)
                 return;
-            a = &dist[k_tile*n+blockIdx.y*GPU_TILE_DIM];
-            b = &dist[k_tile*GPU_TILE_DIM+k_tile*n];
-            c = &dist[k_tile*n+blockIdx.y*GPU_TILE_DIM];
+            a = &dist[k_tile*GPU_TILE_DIM+blockIdx.y*GPU_TILE_DIM*n]; // Tik
+            b = a;
+            c = &dist[k_tile*GPU_TILE_DIM+k_tile*GPU_TILE_DIM*n]; //Tkk
         }
 
     } else { /*case column grid or square grid*/
         
-        if (gridDim.y == 1) { /*column grid*/
+        if (gridDim.y == 1) { /*line grid*/
             if (blockIdx.x == k_tile)
                 return;
-            a = &dist[k_tile*GPU_TILE+blockIdx.x*n];
-            b = &dist[k_tile*GPU_TILE+blockIdx.x*n];
-            c = &dist[k_tile*GPU_TILE_DIM+k_tile*n];
+            a = &dist[k_tile*GPU_TILE_DIM*n+blockIdx.x*GPU_TILE_DIM]; //Tki
+            b = &dist[k_tile*GPU_TILE_DIM+k_tile*GPU_TILE_DIM*n]; //Tkk
+            c = a;
         } else { /*square grid*/
             if ((blockIdx.x == k_tile) || (blockIdx.y == k_tile))
                 return;
-            a = &dist[blockIdx.y*GPU_TILE+blockIdx.x*n];
-            b = &dist[k_tile*GPU_TILE+blockIdx.x*n];
-            c = &dist[blockIdx.y*GPU_TILE_DIM+k_tile*n];
+            a = &dist[blockIdx.y*GPU_TILE_DIM*n+blockIdx.x*GPU_TILE_DIM]; //Tij
+            b = &dist[blockIdx.y*n*GPU_TILE_DIM+k_tile*GPU_TILE_DIM]; //Tik
+            c = &dist[k_tile*GPU_TILE_DIM*n+blockIdx.x*GPU_TILE_DIM]; //Tkj
         }
 
     }
     
-    int row = tid / GPU_TILE;
-    int col = tid % GPU_TILE;
+    int row = tid / GPU_TILE_DIM;
+    int col = tid % GPU_TILE_DIM;
 
-    a[tid] = MIN(a[tid], b[row*n+k]+c[k*n+col]);  
+    a[tid] = MIN(a[tid], b[row*n+kk]+c[kk*n+col]);  
 }
 
 /*
@@ -176,17 +175,34 @@ graph_t *MAKE_KERNEL_NAME(_gpu, _tiled)(graph_t *graph)
     weight_t *dist_gpu = copy_graph_to_gpu(graph);
     timer_stop(&transfer_timer);
 
-    /*
-     * FILLME: Set up and launch the kernel(s)
-     *
-     * You may need different grid/block configurations for each stage
-     * of the computation
-     * 
-     * Use GPU_TILE_DIM (see graph.h) as the tile dimension. You can
-     * adjust its value during compilation. See `make help' for more
-     * information.
-     */
+    int tile_no = graph->nr_vertices / GPU_TILE_DIM;
 
+    for (int kk=0;kk< tile_no;kk++) { // kk = K from the paper, be careful with sizes
+    
+        //phase one
+        dim3 block(GPU_TILE_DIM, GPU_TILE_DIM);
+        dim3 grid1(1);
+        for(int k=0;k<GPU_TILE_DIM;k++) {
+            GPU_KERNEL_NAME(_tiled_shmem)<<<grid1, block>>>(dist_gpu,graph->nr_vertices,k,kk);
+            cudaThreadSynchronize();
+        }
+
+        dim3 grid2(tile_no);
+        dim3 grid3(1,tile_no);
+        for(int k=0;k<GPU_TILE_DIM;k++) {
+            GPU_KERNEL_NAME(_tiled_shmem)<<<grid2, block>>>(dist_gpu,graph->nr_vertices,k,kk);
+            cudaThreadSynchronize();
+            GPU_KERNEL_NAME(_tiled_shmem)<<<grid3, block>>>(dist_gpu,graph->nr_vertices,k,kk);
+            cudaThreadSynchronize();
+        }
+
+        dim3 grid4(tile_no,tile_no);
+        for(int k=0;k<GPU_TILE_DIM;k++) {
+            GPU_KERNEL_NAME(_tiled_shmem)<<<grid4, block>>>(dist_gpu,graph->nr_vertices,k,kk);
+            cudaThreadSynchronize();
+        }
+
+    }
     /*
      * Wait for last kernel to finish, so as to measure correctly the
      * transfer times Otherwise, copy from GPU will block
@@ -234,4 +250,5 @@ graph_t *MAKE_KERNEL_NAME(_gpu, _tiled_shmem)(graph_t *graph)
     printf("Total transfer times: %lf s\n",
            timer_elapsed_time(&transfer_timer));
     return graph;
+            
 }
